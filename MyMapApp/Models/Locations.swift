348536cf77b39a6a392fc50ed41a92019182b50f
timur1011 <timurahmedov1011@gmail.com>
2025-04-08 10:25:13 -0700

    Started create Map App

---
+++ MyMapApp/Models/Locations.swift
@@ -0,0 +1,18 @@
+//
+//  Locations.swift
+//  MyMapApp
+//
+//  Created by 12345 on 08.04.2025.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
