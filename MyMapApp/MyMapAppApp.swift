0aa55bafba2fc13579fe134da949214ef1222a10
timur1011 <timurahmedov1011@gmail.com>
2025-04-08 10:14:14 -0700

    Initial Commit

---
+++ MyMapApp/MyMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapAppApp.swift
+//  MyMapApp
+//
+//  Created by 12345 on 08.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
